ba20fe3b2d89e48446a8130627f81ff4a01d3d8c
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-22 15:18:10 +0300

    Add project files.

---
+++ RecursiveMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.WriteLine("Faktoriyel hesaplamak istediğiniz sayıyı giriniz:");
+            sayi = int.Parse(Console.ReadLine());
+
+            int faktoriyel = FaktoriyelHesapla(sayi);
+            Console.WriteLine(faktoriyel);
+            Console.ReadKey();
+        }
+
+        private static int FaktoriyelHesapla(int sayi)
+        {
+            if (sayi <= 1)
+                return 1;
+            else
+                return sayi * FaktoriyelHesapla(sayi - 1);
+        }
+    }
+}
